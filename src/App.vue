978ac833917ed639e80c287bef9305c2872a225f
moraisLucas <assuncaolucasm@gmail.com>
2022-05-18 17:47:21 -0300

    fix: update vue drag

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +23,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h1 @click="teste">clique aqui</h1>
+    <div id="container">
+      <VueDragResize
+        ref="VueDragResize"
+        :isActive="true"
+        w="auto"
+        h="auto"
+        v-on:resizing="resize"
+        v-on:dragging="resize"
+        :parentLimitation="true"
+        :y="rectTop"
+        :x="rectLeft"
+      >
+        <h3>Hello World!</h3>
+        <p>{{ top }} х {{ left }}</p>
+        <!-- <p>{{ width }} х {{ height }}</p> -->
+      </VueDragResize>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueDragResize from "vue-drag-resize";
+
+export default {
+  name: "vue-drag-resize",
+
+  components: {
+    VueDragResize,
+  },
+
+  data() {
+    return {
+      rectLeft: 200,
+      rectTop: 350,
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
+    };
+  },
+
+  methods: {
+    teste() {
+      this.rectLeft = 0;
+      setTimeout(() => {
+        this.rectTop = 0;
+      });
+
+      this.$forceUpdate();
+    },
+    resize(newRect) {
+      this.width = newRect.width;
+      this.height = newRect.height;
+      this.top = newRect.top;
+      this.left = newRect.left;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.top = 300;
+      this.left = 200;
+    });
+  },
+};
+</script>
+
+<style scoped>
+#container {
+  background: royalblue;
+  width: 800px;
+  height: 500px;
+  position: relative;
+}
+</style>
